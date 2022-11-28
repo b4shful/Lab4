441406a9d710c5acb79698a58e79a4bdbb9c048c
Aisha Shah <aisha@dyn3198-107.wlan.ic.ac.uk>
2022-11-28 16:20:28 +0000

    updated extension

--- sign_extend.sv
+++ sign_extend.sv
@@ -8,9 +8,12 @@ module sign_extend #(
 
 always_comb
     case(ImmSrc):
-        //add
-        1b'0: ImmOp = {20*instr[31], instr[30:0]}
-        //branch
-        1b'1: ImmOp = {31*instr[31], instr[7], instr[30:25], instr[11:8]}
 
+        default: ImmOp = 32b'0
+        //add
+        1b'0: ImmOp = {{21instr[31]}, instr[30:25], instr[24:20]}
+        //branch
+        1b'1: ImmOp = {{31instr[31]}, instr[7], instr[30:25], instr[11:8], 1'b0}
+
+    endcase
 endmodule
