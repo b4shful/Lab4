2e4dff0a6881cd77b12cf0a1de1781d0342b0904
Joel Murphy <joelmurphy444@gmail.com>
2022-11-29 13:34:19 +0000

    Fixed PC
    @tahnimorrison 32'b4 is invalid as 4 isn't a binary number - changed to 32'b100
    line 7 changed pcscr to pcsrc
    added newline to end of file

--- PC.sv
+++ PC.sv
@@ -4,7 +4,7 @@ module PC #(
     //interface signals
     input logic clk,
     input logic rst,
-    input logic [1:0] pcscr,
+    input logic [1:0] pcsrc,
     input logic [WIDTH-1:0] ImmOp,
     output logic [WIDTH-1:0] count
 );
@@ -13,6 +13,6 @@ always_ff @ (posedge clk)
     if (rst) count <= {WIDTH{1'b0}};
 
     else if (pcsrc == 1'b1) count <= count + {ImmOp};
-    else if (pcsrc == 1'b0) count <= count + 32'b4;
+    else if (pcsrc == 1'b0) count <= count + 32'b100;
 
 endmodule 
