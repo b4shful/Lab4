75e67524eb5ead7f4549158f374b1b16c793a886
Aisha Shah <aisha@dyn3205-255.wlan.ic.ac.uk>
2022-12-01 18:19:52 +0000

    changed addressing

--- instr_mem.sv
+++ instr_mem.sv
@@ -1,21 +1,19 @@
 module instr_mem  #(
     parameter   ADDRESS_WIDTH = 32,
-                DATA_WIDTH = 32
+                DATA_WIDTH = 8
 )(
     input  logic    [ADDRESS_WIDTH-1:0]    PC,
     output logic    [DATA_WIDTH-1:0]       instr
 );
 
-logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
+logic [DATA_WIDTH-1:0] rom_array [27:0];
 
 initial begin
         $display("Loading rom.");
         $readmemh("instr_mem.mem", rom_array);
 end;
 
-always_comb
-    //output combinational
-    instr <= rom_array [PC];
-    instr = addr[PC];
-
+always_comb begin
+    assign instr = {{instr_mem[PC]}, {instr_mem[PC+1]}, {instr_mem[PC+2]}, {instr_mem[PC+3]}};
+    end
 endmodule
