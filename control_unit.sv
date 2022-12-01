59d10235017d6210e39e7dcb15973399914f244b
Aisha Shah <aisha@dyn3205-255.wlan.ic.ac.uk>
2022-12-01 18:18:20 +0000

    changed case statement input

--- control_unit.sv
+++ control_unit.sv
@@ -2,7 +2,7 @@ module control_unit(
     input   logic       EQ,
     input   logic       instr[31:0],           
     output  logic       RegWrite,
-    output  logic       ALUctrl,
+    output  logic       ALUctrl[2:0],
     output  logic       ALUsrc,
     output  logic       ImmSrc,
     output  logic       PCsrc     
@@ -10,9 +10,11 @@ module control_unit(
 );
 
 always_comb
-    RegWrite = 0; ALUctrl = 0; ALUsrc = 1; ImmSrc = 0; PCsrc = 0;
+    RegWrite = 0; ALUctrl = 000; ALUsrc = 1; ImmSrc = 0; PCsrc = 0;
 
-    case(instr[6:0], instr[14:12]):
-        7'b0010011 && 3'b000 : RegWrite = 1;
-        7'b1100011 && 3'b001 : PCsrc = 1; ImmSrc = 1;
+    inputinstr = {{instr[6:0]}, {instr[14:12]}}
+    case(inputinstr):
+        10'b0010011000 : RegWrite = 1;
+        10'b1100011001 : PCsrc = 1; ImmSrc = 1;
+    endcase
 endmodule        
